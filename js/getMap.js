dcb84b16e18a12460629e971e58a01d7a780ea33
Kendall H <kenhartman04@gmail.com>
2024-01-16 10:19:32 -0600

    web files added

---
+++ js/getMap.js
@@ -0,0 +1,19 @@
+var map = L.map('map').setView([55.704660, 13.191007], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+var marker = L.marker([55.704660, 13.191007]).addTo(map);
+
+var popup = L.popup();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map);
+}
+
+map.on('click', onMapClick);
