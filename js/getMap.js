8578b08d351dfd8e3e8ce06fde898938200200a1
“kenhartman04” <kenhartman04@gmail.com>
2024-01-24 13:22:12 -0600

    adding geoserver layers

--- js/getMap.js
+++ js/getMap.js
@@ -1,22 +1,28 @@
+// Initialize map
 var map = L.map('map').setView([55.704660, 13.191007], 13);
 
+// Add OpenStreetMap tiles
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-// Add Lantmäteriet's INSPIRE basemap WMS layer
-var wmsLayer = L.tileLayer.wms('https://maps.lantmateriet.se/inspire/basemap/wms/v1', {
-    layers: 'inspire_basemap',
+// Add GeoServer WMS layer
+var wmsLayer = L.tileLayer.wms('http://geoserver.gis.lu.se/geoserver/KenLund/wms', {
+    layers: 'KenLund:bike_path',
     format: 'image/png',
     transparent: true,
-    attribution: 'Map data: Lantmäteriet'
-}).addTo(map);
+    attribution: 'Your attribution here' // Add attribution if required by GeoServer
+});
+wmsLayer.addTo(map);
 
+// Add a marker
 var marker = L.marker([55.704660, 13.191007]).addTo(map);
 
+// Initialize a popup
 var popup = L.popup();
 
+// Function to handle map click event
 function onMapClick(e) {
     popup
         .setLatLng(e.latlng)
@@ -24,4 +30,5 @@ function onMapClick(e) {
         .openOn(map);
 }
 
+// Attach the click event listener to the map
 map.on('click', onMapClick);
